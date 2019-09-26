9768d33d777f882d7245c2a853c2ad00e43aa694
dobrosun <mr.dobrogost02@mail.ru>
2019-09-26 15:25:40 +0300

    refactor in test.sh

--- test.sh
+++ test.sh
@@ -2,13 +2,8 @@
 
 cd test_upload/
 source venv/bin/activate
-
-cd ..
-#make upload
  
-cd -
-pip3 install c_set==0.1.6
+pip3 install --upgrade --force-reinstall c_set
 python3 test.py
 
 deactivate
-

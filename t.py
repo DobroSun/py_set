cd931d07d1b1bef7dac1b670c9c7f133d9e77534
dobrosun <mr.dobrogost02@mail.ru>
2019-09-29 15:20:21 +0300

    add some text in README.md

--- t.py
+++
@@ -1,4 +0,0 @@
-from py_set import pyset
-
-rbt = pyset(5, 10, 1)
-print(rbt.to_list())
--- test_upload/test_compare.py
+++ test_upload/test_compare.py
@@ -5,7 +5,7 @@ import time
 import sys
 import random
 
-random_list = [random.randrange(-1000000, 2000000) for _ in range(100000)]
+random_list = [random.randrange(-1000000, 2000000) for _ in range(1000000)]
 
 pst = pyset()
 st = set()
@@ -16,19 +16,19 @@ print("*** Adding elements ***")
 start = time.time()
 for i in range(1000000):
     lst.append(i)
-end = time.time() - start 
+end = time.time() - start
 print("List time on adding 10^6 items: %.2fms" % (end * 1000))
 
 start = time.time()
 for i in range(1000000):
     st.add(i)
-end = time.time() - start 
+end = time.time() - start
 print("Set time on adding 10^6 items: %.2fms" % (end * 1000))
 
 start = time.time()
 for i in range(1000000):
     pst.add(i)
-end = time.time() - start 
+end = time.time() - start
 print("Pyset time on adding 10^6 items: %.2fms" % (end * 1000))
 print()
 
@@ -36,10 +36,24 @@ print("*** Searching for elements ***")
 start = time.time()
 for i in random_list:
     i in st
-end = time.time() - start 
-print("Set time on searching in 10^5 items: %.2fms" % (end * 1000))
+end = time.time() - start
+print("Set time on searching in 10^6 items: %.2fms" % (end * 1000))
+
 start = time.time()
 for i in random_list:
     i in pst
-end = time.time() - start 
-print("Pyset time on searching in 10^5 items: %.2fms" % (end * 1000))
+end = time.time() - start
+print("Pyset time on searching in 10^6 items: %.2fms" % (end * 1000))
+print()
+
+print("*** Deleting elements ***")
+start = time.time()
+for i in range(1000000):
+    st.remove(i)
+end = time.time() - start
+print("Set time on deleting 10^6 items: %.2fms" % (end * 1000))
+start = time.time()
+for i in range(1000000):
+    pst.remove(i)
+end = time.time() - start
+print("Pyset time on deleting 10^6 items: %.2fms" % (end * 1000))
